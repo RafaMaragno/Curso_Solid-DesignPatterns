3683aea471c14cb1bd51a5060bc6d397fb3eb820
RafaMaragno <rafa.maragno@gmail.com>
2025-02-11 23:59:34 -0300

    Commitando arquivo

---
+++ classSmartDevice.java
@@ -0,0 +1,45 @@
+abstract class Phone {
+    void makeCall();
+}
+
+abstract class EmailDevice {
+    void sendEmail();
+}
+
+abstract class WebBrowser {
+    void browseInternet();
+}
+
+abstract class Camera {
+    void takePicture();
+}
+
+class SmartPhone implements Phone, EmailDevice, WebBrowser, Camera {
+    @Override
+    void makeCall() {
+        print('Making a call');
+    }
+    
+    @Override
+    void sendEmail() {
+        print('Sending an email');
+    }
+    
+    @Override
+    void browseInternet() {
+        print('Browsing in internet');
+    }
+    
+    @Override
+    void takePicture() {
+        print('Taking a picture');
+    }
+
+}
+
+class SmartWatch implements Phone {
+    @Override
+    void makeCall() {
+        print('Making a call');
+    }
+}
